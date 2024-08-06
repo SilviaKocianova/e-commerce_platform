51f0bc37ab2e09a03cd8374e3285f65db48e3064
Silvia Kociánová <sissi.kocianova@gmail.com>
2024-08-06 12:07:17 +0200

    Added Hero

--- src/App.js
+++ src/App.js
@@ -1,36 +1,33 @@
-
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
-import { BrowserRouter as Router, Route, Switch, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Shop from './Pages/Shop';
-
+import Product from './Pages/Product';
+import Cart from './Pages/Cart';
+import ShopCategory from './Pages/ShopCategory';
+import LoginSignup from './Pages/LoginSignup';
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-      <Navbar/>
+      <Router>
+        <Navbar/>
 
-      <Routes>
-        <Route path='/' element={<Shop/>}/>
-        <Route path='/mens' element={<shopCategory category="men"/>}/>
-        <Route path='/womens' element={<shopCategory category="women"/>}/>
-        <Route path='/kids' element={<shopCategory category="kid"/>}/>
+        <Routes>
+          <Route path='/' element={<Shop/>}/>
+          <Route path='/mens' element={<ShopCategory category="men"/>}/>
+          <Route path='/womens' element={<ShopCategory category="women"/>}/>
+          <Route path='/kids' element={<ShopCategory category="kid"/>}/>
 
-        <Route path="/product" element={<Product/>}>
-          <Route path=':productId' element={<Product/>}/>
-        </Route>
-
-      </Routes>
-
-      </BrowserRouter>
-   
-   
+          <Route path="/product">
+            <Route path=':productId' element={<Product/>}/>
+          </Route>
+          <Route path='/cart' element={<Cart/>}/>
+          <Route path='/login' element={<LoginSignup/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
 
 export default App;
-
-
-/*29:08*/
