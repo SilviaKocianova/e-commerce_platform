9781fe11fb937ddf38b263a39687b2a1aa1c6aa5
Silvia Kociánová <sissi.kocianova@gmail.com>
2024-08-11 08:25:24 +0200

    props

--- src/Components/Items/Item.jsx
+++ src/Components/Items/Item.jsx
@@ -6,6 +6,15 @@ const Item = (props) => {
     <div className='item'>
       <img src={props.image} alt="" />
       <p>{props.name}</p>
+      <div className="item-prices">
+        <div className="item-price-new">
+          {props.new_price}
+        </div>
+          {props.old_price}
+        <div className="item-price-old">
+
+        </div>
+      </div>
     </div>
   )
 }
